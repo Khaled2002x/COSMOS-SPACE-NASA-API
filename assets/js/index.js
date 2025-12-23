6492443d850400e1bc62cfaf681ffe536a02da77
Khal2002 <133543703+Khal2002@users.noreply.github.com>
2025-12-23 17:34:58 +0200

    fix window load bugs and add load by data function and add load today image function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 // WRITE YOUR JS CODE HERE
 // apikey=rnIs5dw7lTBxZWyzLT2A6C1dSyzI8rOfdcAlvp4S
-let dom = {
+const dom = {
+  apod_image_container: document.getElementById("apod-image-container"),
   nav_link: document.querySelectorAll(".nav-link"),
   app_section: document.querySelectorAll(".app-section"),
   apod_date_input: document.querySelector("#apod-date-input"),
@@ -12,7 +13,19 @@ let dom = {
   apod_date_detail: document.getElementById("apod-date-detail"),
   apod_explanation: document.getElementById("apod-explanation"),
   apod_copyright: document.getElementById("apod-copyright"),
+  apod_date_info: document.getElementById("apod-date-info"),
+  media_type: document.getElementById("apod-media-type"),
+  load_img: document.querySelector(".load_img"),
+  load_date_btn: document.getElementById("load-date-btn"),
+  today_apod_btn: document.getElementById("today-apod-btn"),
 };
+// auto date
+let current_date = new Date();
+let yaer = current_date.getFullYear();
+let day = current_date.getDate();
+let month = current_date.getMonth() + 1;
+
+dom.apod_date_input.setAttribute("max", `${yaer}-${month}-${day}`);
 dom.apod_date_input.addEventListener("change", () => {
   console.log(dom.apod_date_input.value);
   dom.curr_date.innerHTML = dom.apod_date_input.value;
@@ -36,17 +49,13 @@ dom.nav_link.forEach((nav) => {
 });
 
 // //
-let current_date = new Date();
-let yaer = current_date.getFullYear();
-let day = current_date.getDate();
-let month = current_date.getMonth() + 1;
 
 console.log(`${yaer}-${month}-${day}`);
 
-async function fetchdata_space_today() {
+async function fetchdata_space_today(date) {
   try {
     let data = await fetch(
-      ` https://api.nasa.gov/planetary/apod?api_key=rnIs5dw7lTBxZWyzLT2A6C1dSyzI8rOfdcAlvp4S&date=${dom.apod_date_input.value}`
+      ` https://api.nasa.gov/planetary/apod?api_key=rnIs5dw7lTBxZWyzLT2A6C1dSyzI8rOfdcAlvp4S&date=${date}`
     );
     let response = await data.json();
 
@@ -59,37 +68,53 @@ async function fetchdata_space_today() {
     console.log(error.message);
   }
 }
-// fetchdata_space_today();
-
 dom.apod_date_input.addEventListener("change", () => {
   console.log(dom.apod_date_input.value);
   dom.curr_date.innerHTML = dom.apod_date_input.value;
 });
-async function display_current_space_picture() {
-  let data = await fetchdata_space_today();
+async function display_current_space_picture(date) {
+  let data = await fetchdata_space_today(date);
   dom.live_date.innerHTML = data.date;
   dom.apod_image.src = `${data.hdurl}`;
   dom.apod_title.innerHTML = data.title;
   dom.apod_date_detail.innerHTML = data.date;
   dom.apod_explanation.innerHTML = data.explanation;
   dom.apod_copyright.innerHTML = data.copyright;
+  dom.apod_date_info.innerHTML = data.date;
+  dom.media_type.innerHTML = data.media_type;
 }
 
 window.addEventListener("load", () => {
   let currentsection = localStorage.getItem("section");
+  if (currentsection === null) {
+    document.getElementById("today-in-space").classList.remove("hidden");
+  } else {
+    dom.nav_link.forEach((link) => {
+      link.classList.remove("bg-blue-500/10", "text-blue-400");
+      if (link.dataset.section === currentsection) {
+        link.classList.add("bg-blue-500/10", "text-blue-400");
+      }
+    });
+
+    dom.app_section.forEach((section) => {
+      section.classList.add("hidden");
+    });
+
+    document.querySelector(`#${currentsection}`).classList.remove("hidden");
+  }
+  dom.apod_date_input.value = `${yaer}-${month}-${day}`;
+
+  dom.curr_date.innerHTML = dom.apod_date_input.value;
+  display_current_space_picture(`${yaer}-${month}-${day}`);
+});
+function loadByDate() {
+  let date_value = dom.apod_date_input.value;
+  display_current_space_picture(date_value);
+}
+function loadtodayimage() {
+  display_current_space_picture(`${yaer}-${month}-${day}`);
   dom.apod_date_input.value = `${yaer}-${month}-${day}`;
   dom.curr_date.innerHTML = dom.apod_date_input.value;
-
-  dom.nav_link.forEach((link) => {
-    link.classList.remove("bg-blue-500/10", "text-blue-400");
-    if (link.dataset.section === currentsection) {
-      link.classList.add("bg-blue-500/10", "text-blue-400");
-    }
-  });
-
-  dom.app_section.forEach((section) => {
-    section.classList.add("hidden");
-  });
-  document.querySelector(`#${currentsection}`).classList.remove("hidden");
-  display_current_space_picture();
-});
+}
+dom.today_apod_btn.addEventListener("click", () => loadtodayimage());
+dom.load_date_btn.addEventListener("click", () => loadByDate());
